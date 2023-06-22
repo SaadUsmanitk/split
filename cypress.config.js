61791a3a0b443454e8b3b1822915e3f7b0d2b201
DESKTOP-9JJJ56A\Saadu <Saad.usmani@tkxel.io>
2023-06-22 18:25:49 +0500

    test

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,10 @@ const cypressSplit = require('cypress-split')
 
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
       cypressSplit(on, config)
       return config 
       // implement node event listeners here
